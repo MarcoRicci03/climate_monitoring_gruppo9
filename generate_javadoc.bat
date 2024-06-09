37010859f50377c4b6dbbff42824b81eb49aacb8
MarcoRicci03 <marco.ricci2803@gmail.com>
2024-06-09 16:46:54 +0200

    .

--- generate_javadoc.bat
+++ generate_javadoc.bat
@@ -1,4 +1,4 @@
 @echo off
 set CLASSPATH=lib\AbsoluteLayout.jar;lib\codice-fiscale-java-master.jar;lib\gson-2.8.8.jar;lib\jcalendar-1.4.jar;lib\postgresql-42.7.3.jar;.
-javadoc -d docs -sourcepath .\classi\src\main\java;.\client\src\main\java;.\server\src\main\java -classpath .\libs\* -subpackages classi:org.climate_monitoring_CLIENT:org.climate_monitoring_SERVER
+javadoc -d javadoc -sourcepath .\classi\src\main\java;.\client\src\main\java;.\server\src\main\java -classpath .\libs\* -subpackages classi:org.climate_monitoring_CLIENT:org.climate_monitoring_SERVER
 pause
